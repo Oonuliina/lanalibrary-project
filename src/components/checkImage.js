a6d8f1d92da5590c802c187133f6e81b258c23e4
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2023-05-23 15:35:48 +0300

    back to original

--- src/components/checkImage.js
+++ src/components/checkImage.js
@@ -2,38 +2,29 @@ import React from "react";
 
 let albumCover;
 // tarkastetaan kansikuva albumin nimen perusteella ja palautetaan se
-    export const CheckImage = (result) => {
+    export const CheckImage = (props) => {
 
-        switch (result.album) {
-            case "Born to Die": 
-                albumCover = "borntodie.png";
-                break;
-            case "Born to Die: The Paradise Edition": 
-                albumCover = "paradise.png";
-                break;
-            case "Ultraviolence": 
-                albumCover = "ultraviolence.png";
-                break;
-            case "Honeymoon": 
-                albumCover = "honeymoon.png";
-                break;
-            case "Lust for Life": 
-                albumCover = "lustforlife.png";
-                break;
-            case "Norman Fucking Rockwell": 
-                albumCover = "nfr.png";
-                break;
-            case "Chemtrails over the Country Club": 
-                albumCover = "chemtrails.png";
-                break;
-            case "Blue Banisters": 
-                albumCover = "bluebanisters.png";
-                break;
-            case "Did You Know That There's a Tunnel Under Ocean Blvd": 
-                albumCover = "oceanblvd.png";
-                break;                                                     
-            default: albumCover = "placeholder.png";
-        }
+        if (props.album === "Born to Die") {
+            albumCover = "borntodie.png";
+        } else if (props.album === "Born to Die: The Paradise Edition") {
+            albumCover = "paradise.png";
+        } else if (props.album === "Ultraviolence") {
+            albumCover = "ultraviolence.png";
+        } else if (props.album === "Honeymoon") {
+            albumCover = "honeymoon.png";
+        } else if (props.album === "Lust for Life") {
+            albumCover = "lustforlife.png";
+        } else if (props.album === "Norman Fucking Rockwell") {
+            albumCover = "nfr.png";
+        } else if (props.album === "Chemtrails over the Country Club") {
+            albumCover = "chemtrails.png";
+        } else if (props.album === "Blue Banisters") {
+            albumCover = "bluebanisters.png";
+        } else if (props.album === "Did You Know That There's a Tunnel Under Ocean Blvd") {
+            albumCover = "oceanblvd.png";
+        } else {
+            albumCover = "placeholder.png";
+        }  
 
         return (
             <img src={albumCover} alt="Album cover"/>    
